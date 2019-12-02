8921965bb105a2d69450158a150c3a4990c5dcf4
hemasundar2 <reddy.hemasundar@gmail.com>
2019-12-02 13:37:22 -0600

    Update pullRequestPipeline.groovy

--- vars/pullRequestPipeline.groovy
+++ vars/pullRequestPipeline.groovy
@@ -8,7 +8,7 @@ def call(Object name, String database, String url, String id) {
             echo 'Hello World'+id_database
 		               withCredentials([usernamePassword(credentialsId: '5f71cca3-f7b0-416a-b799-afb61fa4bb9e', passwordVariable: 'DB_PASSWORD', usernameVariable: 'DB_USER')]) {
 				       echo 'Hello World 1'+DB_USER
-	     createDatabase(DB_USER,DB_PASSWORD,database, url)
+	    // createDatabase(DB_USER,DB_PASSWORD,database, url)
 }
               
          }
