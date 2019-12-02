9ef303969fb4635ce0d0574d762096e93da620ad
hemasundar2 <reddy.hemasundar@gmail.com>
2019-12-01 18:50:28 -0600

    Update pullRequestPipeline.groovy

--- vars/pullRequestPipeline.groovy
+++ vars/pullRequestPipeline.groovy
@@ -6,6 +6,9 @@ def call(Object name) {
       stage('Hello') {
          steps {
             echo 'Hello World'+name
+		               withCredentials([usernamePassword(credentialsId: 'e5f8e3f0-dd6f-44c8-a89d-29375ea34cd9', passwordVariable: 'PASSWORD', usernameVariable: 'USER_NAME')]) {
+             echo 'Hello World 1'+USER_NAME
+              }
          }
       }
    }
