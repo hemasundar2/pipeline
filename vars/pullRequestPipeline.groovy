edb98cce3ed389150c9578f3400c67fdb227ddb5
hemasundar2 <reddy.hemasundar@gmail.com>
2019-12-01 21:47:15 -0600

    Update pullRequestPipeline.groovy

--- vars/pullRequestPipeline.groovy
+++ vars/pullRequestPipeline.groovy
@@ -26,7 +26,7 @@ def createDatabase(String user, String password){
          Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
      
           vsql = Sql.newInstance("jdbc:sqlserver://hrsabmssql1dev.nih.gov", user,password, "com.microsoft.sqlserver.jdbc.SQLServerDriver")
-    
+    echo 'Hello World 2'
           //vsql.execute("create database LIQUIBASE_UMS_CORE")
           vsql.close()  
 
