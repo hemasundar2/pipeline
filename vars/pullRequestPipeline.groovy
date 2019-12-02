17af45d3e8ec899bf47488e0cecf3e967eb87c80
hemasundar2 <reddy.hemasundar@gmail.com>
2019-12-02 13:48:01 -0600

    Update pullRequestPipeline.groovy

--- vars/pullRequestPipeline.groovy
+++ vars/pullRequestPipeline.groovy
@@ -1,11 +1,15 @@
 def call(Object name, String database, String url ) {
 	pipeline {
    agent any
-   id = 2
+   environment {
+			id = 2
+			databaseName = id+'_'+database+'_CORE'
+		}
    stages {
       stage('Hello') {
          steps {
             echo 'Hello World'+id+"_"+database
+		  echo 'database name'+id+"_"+database
 		               withCredentials([usernamePassword(credentialsId: '5f71cca3-f7b0-416a-b799-afb61fa4bb9e', passwordVariable: 'DB_PASSWORD', usernameVariable: 'DB_USER')]) {
 				       echo 'Hello World 1'+DB_USER
 	    // createDatabase(DB_USER,DB_PASSWORD,database, url)
