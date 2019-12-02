259879848aa328b9bb6f08b6276a7649d7dca0c8
hemasundar2 <reddy.hemasundar@gmail.com>
2019-12-02 17:38:22 -0600

    Update pullRequestPipeline.groovy

--- vars/pullRequestPipeline.groovy
+++ vars/pullRequestPipeline.groovy
@@ -2,10 +2,17 @@ def call(Object name, String database, String context ) {
 	pipeline {
 		agent any
 
+		options {
 
-		environment {
-			boolean skipBuild = false
 		}
+
+		parameters {
+			
+		}
+
+environment {
+skipBuild = false
+}
 
 		stages {
 			stage ('Checkout') {
@@ -26,15 +33,40 @@ def call(Object name, String database, String context ) {
 				}
 
 				steps{
-				echo "Create database"	
-					printTest()
+					
 					}
 				}
 
 			}
 
+			stage ("Build Database") {
+				when {
+					// environment variables are always stored as Strings and need to be converted
+					expression {!skipBuild.toBoolean()}
+				}
+
+				steps {
+					echo "Notifying Bitbucket of build in progress"
+
+				}
+
+				post {
+					always {
+						
+					}
+
+					success {
+						// Build result needs to be updated for bitbucket notification to work; see https://github.com/jenkinsci/stashnotifier-plugin
+
+
+					}
+
+					failure {
+						// Build result needs to be updated for bitbucket notification to work; see https://github.com/jenkinsci/stashnotifier-plugin
+
+					}
+				}
+			}
 		}
 	}
-def printTest(){
-	echo "Create database 1"	
-}
+
