a387db551438878a2c3a8025b6442c2e19d9fdef
hemasundar2 <reddy.hemasundar@gmail.com>
2019-12-11 11:57:17 -0600

    Update pullRequestPipeline.groovy

--- vars/pullRequestPipeline.groovy
+++ vars/pullRequestPipeline.groovy
@@ -2,7 +2,10 @@ def call(Object name, String database, String env,String dburl ) {
 	pipeline {
 		agent any
 
-
+		parameters {
+			string defaultValue: '', description: 'The target revision being merged into', name: 'database', trim: false
+			string defaultValue: '', description: 'The source revision for the merge', name: 'PULL_REQUEST_ID', trim: false
+			}
 		environment {
 			boolean skipBuild = false
 		}
